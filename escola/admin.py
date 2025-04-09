188ec06b409f1d1ab602d9fe635cac71543a77e3
Kkraties13 <160873692+Kkraties13@users.noreply.github.com>
2025-04-09 12:09:53 -0300

    Novos campos
    Falta adicionar o contrato

--- escola/admin.py
+++ escola/admin.py
@@ -8,22 +8,22 @@ class TurmasAdmin(admin.ModelAdmin):
     list_filter = ('itinerario', 'turma')
 
 class ResponsaveisAdmin(admin.ModelAdmin):
-    list_display = ('name', 'last_name', 'phone_number', 'email', 'address', 'cpf', 'birthday')
-    list_display_links = ('name', 'last_name', 'phone_number', 'email', 'address', 'cpf', 'birthday')
-    search_fields = ('name', 'last_name')
-    list_filter = ('name', 'last_name')
+    list_display = ('name_responsavel', 'phone_number_responsavel', 'email_responsavel', 'address_responsavel', 'cpf_responsavel', 'birthday_responsavel', 'classes_filhos')
+    list_display_links = ('name_responsavel', 'phone_number_responsavel', 'email_responsavel', 'address_responsavel', 'cpf_responsavel', 'birthday_responsavel')
+    search_fields = ('name_responsavel','name_responsavel', 'phone_number_responsavel', 'email_responsavel', 'cpf_responsavel')
+    list_filter = ('name_responsavel', 'cpf_responsavel', 'classes_filhos')
     
 class AlunoAdmin(admin.ModelAdmin):
-    list_display = ('name_aluno', 'last_name_aluno', 'phone_number_aluno', 'email_aluno', 'address_aluno', 'cpf_aluno', 'birthday_aluno', 'class_aluno')
-    list_display_lubjs = ('name_aluno', 'last_name_aluno', 'phone_number_aluno', 'email_aluno', 'address_aluno', 'cpf_aluno', 'birthday_aluno', 'class_aluno')
-    search_fields = ('name_aluno', 'last_name_aluno', 'phone_number_aluno', 'email_aluno', 'address_aluno', 'cpf_aluno', 'birthday_aluno', 'class_aluno')
-    list_filter = ('name_aluno', 'last_name_aluno')
+    list_display = ('name_aluno', 'phone_number_aluno', 'email_aluno', 'address_aluno', 'cpf_aluno', 'birthday_aluno', 'class_aluno')
+    list_display_links = ('name_aluno', 'phone_number_aluno', 'email_aluno', 'address_aluno', 'cpf_aluno', 'birthday_aluno', 'class_aluno')
+    search_fields = ('name_aluno', 'phone_number_aluno', 'email_aluno', 'address_aluno', 'cpf_aluno', 'birthday_aluno', 'class_aluno')
+    list_filter = ('name_aluno', 'cpf_aluno', 'class_aluno')
     
 class ProfessorAdmin(admin.ModelAdmin):
-    list_display = ('name_professor', 'last_name_professor', 'phone_number_professor', 'email_professor', 'address_professor', 'cpf_professor', 'birthday_professor', 'subject', 'classes')
-    list_display_links = ('name_professor', 'last_name_professor', 'phone_number_professor', 'email_professor', 'address_professor', 'cpf_professor', 'birthday_professor', 'subject', 'classes')
-    search_fields = ('name_professor', 'last_name_professor', 'phone_number_professor', 'email_professor', 'address_professor', 'cpf_professor', 'birthday_professor', 'subject', 'classes')
-    list_filter = ('name_professor', 'last_name_professor')
+    list_display = ('name_professor', 'phone_number_professor', 'email_professor', 'address_professor', 'cpf_professor', 'birthday_professor', 'subject', 'classes')
+    list_display_links = ('name_professor', 'phone_number_professor', 'email_professor', 'address_professor', 'cpf_professor', 'birthday_professor', 'subject', 'classes')
+    search_fields = ('name_professor', 'phone_number_professor', 'email_professor', 'address_professor', 'cpf_professor', 'birthday_professor', 'subject', 'classes')
+    list_filter = ('name_professor', 'subject', 'classes')
     
 
 admin.site.register(Turmas, TurmasAdmin)
--- escola/models.py
+++ escola/models.py
@@ -30,75 +30,73 @@ def validador(cpf):
 
 class Turmas(models.Model):
     TURMAS_CHOICES = (
-        "1A", "1º Ano A",
-        "1B", "1º Ano B",
-        "1C", "1º Ano C",
-        "2A", "2º Ano A",
-        "2B", "2º Ano B",
-        "2C", "2º Ano C",
-        "3A", "3º Ano A",
-        "3B", "3º Ano B",
-        "3C", "3º Ano C",
+        ("1A", "1º Ano A"),
+        ("1B", "1º Ano B"),
+        ("1C", "1º Ano C"),
+        ("2A", "2º Ano A"),
+        ("2B", "2º Ano B"),
+        ("2C", "2º Ano C"),
+        ("3A", "3º Ano A"),
+        ("3B", "3º Ano B"),
+        ("3C", "3º Ano C"),
     )
     
     ITINERARIO_CHOICES = (
-        "DS", "Desenvolvimento de Sistemas",
-        "CN", "Ciências da Natureza",
-        "JG", "Desenvolvimento de Jogos",
+        ("DS", "Desenvolvimento de Sistemas"),
+        ("CN", "Ciências da Natureza"),
+        ("JG", "Desenvolvimento de Jogos"),
     )
     
     sala = models.CharField(max_length=2, verbose_name="Sala")
     padrinho = models.CharField(max_length=50, verbose_name="Nome do Padrinho")
     representante = models.CharField(max_length=50, verbose_name="Nome do Representante")
     vice_representante = models.CharField(max_length=50, verbose_name="Nome do Vice Representante")
-    itinerario = models.Choices(ITINERARIO_CHOICES, verbose_name="Itinerário")
-    turma = models.Choices(TURMAS_CHOICES, verbose_name="Turma")
+    itinerario = models.CharField(max_length=2, choices=ITINERARIO_CHOICES, verbose_name="Itinerario")
+    turma = models.CharField(max_length=2, choices=TURMAS_CHOICES, verbose_name="Turma")
     
     def __str__(self):
         return self.turma + "" + self.itinerario 
 
 class ResponsavelAluno(models.Model):
     TURMAS_CHOICES = (
-        "1A", "1º Ano A",
-        "1B", "1º Ano B",
-        "1C", "1º Ano C",
-        "2A", "2º Ano A",
-        "2B", "2º Ano B",
-        "2C", "2º Ano C",
-        "3A", "3º Ano A",
-        "3B", "3º Ano B",
-        "3C", "3º Ano C",
+        ("1A", "1º Ano A"),
+        ("1B", "1º Ano B"),
+        ("1C", "1º Ano C"),
+        ("2A", "2º Ano A"),
+        ("2B", "2º Ano B"),
+        ("2C", "2º Ano C"),
+        ("3A", "3º Ano A"),
+        ("3B", "3º Ano B"),
+        ("3C", "3º Ano C"),
     )
     
     name_responsavel = models.CharField(max_length=50)
-    last_name_responsavel = models.CharField(max_length=50)
-    phone_number_responsavel = models.CharField(max_length=15, verbose_name="Digite seu número de telefone")
+    phone_number_responsavel = models.CharField(max_length=15, verbose_name="Telefone do responsável")
     email_responsavel = models.EmailField(max_length=100, verbose_name="E-Mail do responsavel")
     address_responsavel = models.CharField(max_length=100)
     cpf_responsavel = models.CharField(max_length=11, unique=True)
     birthday_responsavel = models.DateField()
-    classes_filhos = models.Choices(TURMAS_CHOICES)
+    classes_filhos = models.CharField(max_length=2, choices=TURMAS_CHOICES)
     
 
     def __str__(self):
-        return self.name_responsavel + " " + self.last_name_responsavel
+        return self.name_responsavel
     
 class Aluno(models.Model):
     TURMA_CHOICES = (
-        "1A", "1º Ano A",
-        "1B", "1º Ano B",
-        "1C", "1º Ano C",
-        "2A", "2º Ano A",
-        "2B", "2º Ano B",
-        "2C", "2º Ano C",
-        "3A", "3º Ano A",
-        "3B", "3º Ano B",
-        "3C", "3º Ano C",
+        ("1A", "1º Ano A"),
+        ("1B", "1º Ano B"),
+        ("1C", "1º Ano C"),
+        ("2A", "2º Ano A"),
+        ("2B", "2º Ano B"),
+        ("2C", "2º Ano C"),
+        ("3A", "3º Ano A"),
+        ("3B", "3º Ano B"),
+        ("3C", "3º Ano C"),
     )
     
-    name_aluno = models.CharField(max_length=50)
-    last_name_aluno = models.CharField(max_length=50)
-    phone_number_aluno = models.CharField(max_length=15, verbose_name="Digite seu número de telefone")
+    name_aluno = models.CharField(max_length=200)
+    phone_number_aluno = models.CharField(max_length=15, verbose_name="Telefone do aluno")
     email_aluno = models.EmailField(max_length=100, verbose_name="E-Mail do Aluno")
     address_aluno = models.CharField(max_length=100)
     cpf_aluno = models.CharField(max_length=11, unique=True)
@@ -114,38 +112,37 @@ class Aluno(models.Model):
             blank=False
         )
     
-    contrato = models.OneToOneField(
-        on_delete=models.CASCADE,
-        related_name='aluno',
-        null=True,
-        blank=True
-    )
+    # contrato = models.OneToOneField(
+    #     on_delete=models.CASCADE,
+    #     related_name='aluno',
+    #     null=True,
+    #     blank=True
+    # )
     
     def __str__(self):
-        return self.name_aluno + " " + self.last_name_aluno + " - " + self.class_aluno
+        return f"{self.name_aluno} - {self.class_aluno}"
     
 class Professor(models.Model):
     TURMAS_CHOICES = (
-        "1A", "1º Ano A",
-        "1B", "1º Ano B",
-        "1C", "1º Ano C",
-        "2A", "2º Ano A",
-        "2B", "2º Ano B",
-        "2C", "2º Ano C",
-        "3A", "3º Ano A",
-        "3B", "3º Ano B",
-        "3C", "3º Ano C",
+        ("1A", "1º Ano A"),
+        ("1B", "1º Ano B"),
+        ("1C", "1º Ano C"),
+        ("2A", "2º Ano A"),
+        ("2B", "2º Ano B"),
+        ("2C", "2º Ano C"),
+        ("3A", "3º Ano A"),
+        ("3B", "3º Ano B"),
+        ("3C", "3º Ano C"),
     )
     
-    name_professor = models.CharField(max_length=50)
-    last_name_professor = models.CharField(max_length=50)
-    phone_number_professor = models.CharField(max_length=15, verbose_name="Digite seu número de telefone")
+    name_professor = models.CharField(max_length=200)
+    phone_number_professor = models.CharField(max_length=15, verbose_name="Telefone do professor")
     email_professor = models.EmailField(max_length=100, verbose_name="E-Mail do Professor")
     address_professor = models.CharField(max_length=100)
     cpf_professor = models.CharField(max_length=11, unique=True)
     birthday_professor = models.DateField()
     subject = models.CharField(max_length=50)
-    classes = models.Choices(TURMAS_CHOICES)
+    classes = models.CharField(max_length=2, choices=TURMAS_CHOICES)
     
     def __str__(self):
         return self.name_professor
